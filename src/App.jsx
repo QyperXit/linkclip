e5ed2973a6e28b9f355f04a70a3ecf173125f0d3
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-07-02 10:14:18 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className=" underline text-2xl">test</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
