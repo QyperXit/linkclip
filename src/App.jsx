7a40b5f013627a21858a9b08f3d82887e40ba333
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-07-02 13:39:45 +0100

    router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
+import { createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import AppLayout from "./layouts/app-layout";
+import Auth from "./pages/auth";
+import Dashboard from "./pages/dashboard";
+import LandingPage from "./pages/landingPage";
+import Link from "./pages/link";
+import Redirect from "./pages/redirect";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <LandingPage />,
+        },
+        {
+          path: "/dashboard",
+          element: <Dashboard />,
+        },
+        {
+          path: "/auth",
+          element: <Auth />,
+        },
+        {
+          path: "/link/:id",
+          element: <Link />,
+        },
+        {
+          path: "/:id",
+          element: <Redirect />,
+        },
+      ],
+    },
+  ]);
+
   return (
     <>
       <div>
---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <main>
+        {/* header */}
+        {/* body */}
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+};
+
+export default AppLayout;
