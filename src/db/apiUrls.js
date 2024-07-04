0f4f341a9f83c2057bb6a4827dd48555212c2907
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-07-05 00:20:41 +0100

    createUrl-logic

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -14,16 +14,36 @@ export async function getUrls(user_id) {
   return data;
 }
 
-export async function deleteUrl(id) {
-  const { data, error } = await supabase.from("urls").delete().eq("id", id);
+export async function getUrl({ id, user_id }) {
+  const { data, error } = await supabase
+    .from("urls")
+    .select("*")
+    .eq("id", id)
+    .eq("user_id", user_id)
+    .single();
 
   if (error) {
     console.error(error);
-    throw new Error("Unable to delete Url");
+    throw new Error("Short Url not found");
   }
 
   return data;
 }
+
+// export async function getLongUrl(id) {
+//   let { data: shortLinkData, error: shortLinkError } = await supabase
+//     .from("urls")
+//     .select("id, original_url")
+//     .or(`short_url.eq.${id},custom_url.eq.${id}`)
+//     .single();
+
+//   if (shortLinkError && shortLinkError.code !== "PGRST116") {
+//     console.error("Error fetching short link:", shortLinkError);
+//     return;
+//   }
+
+//   return shortLinkData;
+// }
 
 export async function createUrl(
   { title, longUrl, customUrl, user_id },
@@ -57,6 +77,17 @@ export async function createUrl(
   if (error) {
     console.error(error);
     throw new Error("Error creating short URL");
+  }
+
+  return data;
+}
+
+export async function deleteUrl(id) {
+  const { data, error } = await supabase.from("urls").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Unable to delete Url");
   }
 
   return data;
