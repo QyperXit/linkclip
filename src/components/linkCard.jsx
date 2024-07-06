563d7e18f796df39f53649b73cae1746286015f2
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-07-06 01:06:52 +0100

    link test

--- src/components/linkCard.jsx
+++ src/components/linkCard.jsx
@@ -41,7 +41,7 @@ const LinkCard = ({ url, fetchUrls }) => {
           {url?.title}
         </span>
         <span className="text-2xl text-blue-400 font-bold hover:underline cursor-pointer">
-          https://linkclip.in/
+          https://linkclip.netlify.app
           {url?.custom_url ? url?.custom_url : url.short_url}
         </span>{" "}
         <span className="flex items-center gap-1 hover:underline cursor-pointer">
@@ -57,7 +57,7 @@ const LinkCard = ({ url, fetchUrls }) => {
           variant="ghost"
           onClick={() =>
             navigator.clipboard.writeText(
-              `https://linkclip.in/${url?.short_url}`
+              `https://linkclip.netlify.app/${url?.short_url}`
             )
           }
         >
